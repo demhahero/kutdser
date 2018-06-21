466118762929fe1379f479c6ea88a2c6df13e269
WaelMohammedAbed <waelrealmadrid@gmail.com>
2018-06-21 20:13:54 +0300

    update request_details page and add all the information required

--- mikrotik/requests/request_details.php
+++ mikrotik/requests/request_details.php
@@ -4,70 +4,460 @@ include_once "../header.php";
 
 <?php
 $request_id = intval($_GET["request_id"]);
-$request = $dbTools->objRequestTools($request_id);
+
+// get request info and reseller info
+$query="SELECT `request_id`, reseller.`customer_id`, `admins`.`username`
+,reseller.`full_name`, `requests`.`order_id`, `creation_date`, `action`, `action_value`,`admins`.`admin_id`, `verdict`, `verdict_date`, `action_on_date`, `product_price`, `requests`.`note`, `product_title`, `product_category`, `product_subscription_type`, `modem_mac_address`
+FROM `requests`
+INNER JOIN `customers` as reseller on `reseller`.`customer_id`= `requests`.`reseller_id`
+LEFT JOIN `admins` on `admins`.`admin_id`=`requests`.`admin_id`
+WHERE `request_id`=".$request_id;
+$request = $dbTools->query($query);
+$request_row=$dbTools->fetch_assoc($request);
+
+/// get request's order info
+$request_order_query="SELECT *,`customers`.`full_name` FROM `orders`
+INNER JOIN `order_options` on `order_options`.`order_id`=`orders`.`order_id`
+INNER JOIN `customers` on `customers`.`customer_id`=`orders`.`customer_id`
+where `orders`.`order_id`=".$request_row['order_id'];
+$request_order=$dbTools->query($request_order_query);
+$request_order_row=$dbTools->fetch_assoc($request_order);
+/// indentify start active date
+$start_active_date="";
+if($request_order_row["product_category"]==="phone"){
+  $start_active_date=$request_order_row["creation_date"];
+}
+else if($request_order_row["product_category"]==="internet"){
+  if($request_order_row["cable_subscriber"]==="yes"){
+    $start_active_date=$request_order_row["cancellation_date"];
+  }
+  else {
+    $start_active_date=$request_order_row["installation_date_1"];
+  }
+}
+
+/// get last approved request for this order if exist;
+$last_request_query="SELECT `request_id`, reseller.`customer_id`, `admins`.`username`
+,reseller.`full_name`, `requests`.`order_id`, `creation_date`, `action`, `action_value`,`admins`.`admin_id`, `verdict`, `verdict_date`, `action_on_date`, `product_price`, `requests`.`note`, `product_title`, `product_category`, `product_subscription_type`, `modem_mac_address`
+FROM `requests`
+INNER JOIN `customers` as reseller on `reseller`.`customer_id`= `requests`.`reseller_id`
+LEFT JOIN `admins` on `admins`.`admin_id`=`requests`.`admin_id`
+WHERE `requests`.`order_id`=".$request_row['order_id']." and verdict='approve' ORDER BY action_on_date DESC LIMIT 1 OFFSET 1";
+$last_request=$dbTools->query($last_request_query);
+$last_request_row=$dbTools->fetch_assoc($last_request);
+
 
 if (isset($_POST["verdict"])) {
 
-    $request->setVerdict($_POST["verdict"]);
+
     $verdict_date = new DateTime();
-    $admin = $dbTools->objAdminTools($admin_id);
-    $request->setAdmin($admin);
-    $request->setVerdictDate($verdict_date);
-    $request_result = $request->doUpdate();
+
+    $query_update_request="UPDATE `requests` SET
+    `admin_id`=N'".$admin_id."',
+    `verdict`=N'".$_POST["verdict"]."',
+    `verdict_date`=N'".$verdict_date->format('Y-m-d')."'
+    WHERE `requests`.`request_id`=".$_POST["request_id"];
+    $request_result = $dbTools->query($query_update_request);
 
     if ($request_result) {
         echo "<script>window.location.href = \"" . $site_url . "/requests/requests.php\";</script>";
     }
 }
+
 ?>
 
 <title>Request Details</title>
 <div class="page-header">
-    <h4>Request Details</h4>    
+    <h4>Request Details</h4>
 </div>
 
 <br>
 
 
 <?php
-if ($request->getVerdict() == "") {
+
+if ($request_row["verdict"] == "") {
     ?>
     <form class="register-form" method="post">
         <div class="form-group">
             <label for="email">Note:</label>
-            <?= $request->getNote() ?>
+            <?= $request_row['note'] ?>
         </div>
         <div class="form-group">
+          <input type="hidden" name="request_id" value="<?=$request_id?>"/>
             <label for="email">Verdict:</label>
             <select  name="verdict" class="form-control">
-                <option <?php if ($request->getVerdict() == "approve") echo "selected"; ?> value="approve">approve</option>
-                <option <?php if ($request->getVerdict() == "disapprove") echo "selected"; ?> value="disapprove">disapprove</option>
+                <option  value="approve">approve</option>
+                <option  value="disapprove">disapprove</option>
             </select>
         </div>
         <input type="submit" class="btn btn-default" value="Submit">
     </form>
     <?php
-} else {
+} else
+{
     ?>
     <div class="form-group">
         <label for="email">Note:</label>
-        <?= $request->getNote() ?>
+        <?= $request_row["note"] ?>
     </div>
+
     <div>
         <table class="display table table-striped table-bordered">
             <tr>
-                <td>                     
-                    "<?= $request->getAdmin()->getUsername() ?>" 
-                    <?= $request->getVerdict() ?> 
-                    on 
-                    <?= $request->getVerdictDate() ?>
+                <td>
+                    "<?= $request_row["username"] ?>"
+                    <?= $request_row["verdict"] ?>
+                    on
+                    <?= $request_row["verdict_date"] ?>
                 </td>
             </tr>
         </table>
     </div>
-    <?php
-}
-?>
+    <?PHP }?>
+    <div class="row" style="width:100% !important;">
+      <div class="col-lg-12 col-md-12 col-sm-12" >
+          <p class="rounded form-row form-row-wide">
+          <div class="panel panel-success">
+              <div class="panel-heading">Request Info</div>
+              <div class="panel-body">
+                <table class="display table table-striped table-bordered">
+                  <?PHP
+                  if(sizeof($request_row)>0){
+                  ?>
+                  <tr>
+                      <td>Action:</td>
+                      <td>
+                          <?= $request_row['action'] ?>
+                      </td>
+
+                      <td>Action On Date:</td>
+                      <td>
+                          <?= $request_row['action_on_date'] ?>
+                      </td>
+
+                      <td>Admin:</td>
+                      <td>
+                          <?= $request_row['username'] ?>
+                      </td>
+
+                      <td>Verdict:</td>
+                      <td >
+                          <?= $request_row['verdict'] ?>
+                      </td>
+
+                      <td colspan="2">Verdict Date:</td>
+                      <td colspan="2">
+                          <?= $request_row['verdict_date'] ?>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Reseller Name:</td>
+                      <td>
+                          <?= $request_row['full_name'] ?>
+                      </td>
+
+                      <td>Product Name:</td>
+                      <td>
+                          <?= $request_row['product_title'] ?>
+                      </td>
+
+                      <td>Product price:</td>
+                      <td>
+                          <?= $request_row['product_price'] ?>
+                      </td>
+
+                      <td>Product Type:</td>
+                      <td>
+                          <?= $request_row['product_subscription_type'] ?>
+                      </td>
+
+                      <td>Product Category:</td>
+                      <td>
+                          <?= $request_row['product_category'] ?>
+                      </td>
+
+                      <td>Note:</td>
+                      <td>
+                          <?= $request_row['note'] ?>
+                      </td>
+                  </tr>
+                  <?PHP }
+                  else{
+                    ?>
+                    <tr>
+                        <td>There are no previous Requests</td>
+
+                    </tr>
+                    <?PHP
+                  }?>
+                </table>
+              </div>
+          </div>
+          </p>
+      </div>
+    </div>
+    <div class="row" style="width:100% !important;">
+        <div class="col-lg-6 col-md-6 col-sm-12" >
+            <p class="rounded form-row form-row-wide">
+            <div class="panel panel-success">
+                <div class="panel-heading">Order Info</div>
+                <div class="panel-body">
+                  <table class="display table table-striped table-bordered">
+                    <tr>
+                        <td>Customer Name</td>
+                        <td>
+                            <?=$request_order_row['full_name'] ?>
+                        </td>
+                    </tr>
+                      <tr>
+                          <td style="width:20%;">order ID</td>
+                          <td><?PHP
+
+                          if((int)$request_order_row['order_id'] <= 10380)
+                              {echo  $request_order_row['order_id'];}
+                          else {
+                            echo (((0x0000FFFF & (int)$request_order_row['order_id']) << 16) + ((0xFFFF0000 & (int)$request_order_row['order_id']) >> 16));
+                          }
+
+                           ?></td>
+                      </tr>
+                      <tr>
+                          <td>Start Active Date</td>
+                          <td>
+                              <?=$start_active_date ?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Product Type</td>
+                          <td>
+                              <?=$request_order_row['product_subscription_type'] ?>
+                          </td>
+                      </tr>
+                              <tr>
+                          <td>Product Category</td>
+                          <td>
+                              <?= $request_order_row['product_category'] ?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Product Name</td>
+                          <td>
+                              <?= $request_order_row['product_title'] ?>
+                          </td>
+
+                      <tr>
+                          <td>Creation Date</td>
+                          <td><?=$request_order_row['creation_date'] ?></td>
+                      </tr>
+                      <tr>
+                          <td>Status</td>
+                          <td><?=$request_order_row['status'] ?></td>
+                      </tr>
+
+                      <tr>
+                          <td>Plan</td>
+                          <td>
+                              <?=$request_order_row['plan'] ?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Modem</td>
+                          <td>
+                              <?=$request_order_row['modem'] ?>
+                          </td>
+                      </tr>
+
+                      <tr>
+                          <td>Router</td>
+                          <td>
+                              <?=$request_order_row['router'] ?>
+                          </td>
+                      </tr>
+                      <?php
+                      if ($request_order_row['cable_subscriber']  == "yes") {
+                          ?>
+                          <tr>
+                              <td>Cable subscriber</td>
+                              <td>
+                                  <?=$request_order_row['cable_subscriber']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>Current cable provider</td>
+                              <td>
+                                  <?=$request_order_row['current_cable_provider']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>Cancellation date</td>
+                              <td>
+                                  <?=$request_order_row['cancellation_date']?>
+                              </td>
+                          </tr>
+                          <?php
+                      } else if ($request_order_row['cable_subscriber'] == "no") {
+                          ?>
+                          <tr>
+                              <td>installation date 1</td>
+                              <td>
+                                  <?=$request_order_row['installation_date_1']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>installation time 1</td>
+                              <td>
+                                  <?=$request_order_row['installation_time_1']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>installation date 2</td>
+                              <td>
+                                  <?=$request_order_row['installation_date_2']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>installation time 2</td>
+                              <td>
+                                  <?=$request_order_row['installation_time_2']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>installation date 3</td>
+                              <td>
+                                  <?=$request_order_row['installation_date_3']?>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td>installation time 3</td>
+                              <td>
+                                  <?=$request_order_row['installation_time_3']?>
+                              </td>
+                          </tr>
+                          <?php
+                      }
+                      ?>
+                      <tr>
+                          <td>additional service</td>
+                          <td>
+                              <?=$request_order_row['additional_service']?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Actual installation date:</td>
+                          <td>
+                              <?php if($request_order_row['actual_installation_date']) echo $request_order_row['actual_installation_date']; ?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Actual installation time from:</td>
+                          <td>
+                              <?= $request_order_row['actual_installation_time_from']; ?>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>Actual installation time to:</td>
+                          <td>
+                              <?= $request_order_row['actual_installation_time_to']; ?>
+                          </td>
+                      </tr>
+                  </table>
+                </div>
+            </div>
+            </p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12" >
+            <p class="rounded form-row form-row-wide">
+            <div class="panel panel-success">
+                <div class="panel-heading">Previous Request Info</div>
+                <div class="panel-body">
+                  <table class="display table table-striped table-bordered">
+                    <?PHP
+                    if(sizeof($last_request_row)>0){
+                    ?>
+                    <tr>
+                        <td>Action:</td>
+                        <td>
+                            <?= $last_request_row['action'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Action On Date:</td>
+                        <td>
+                            <?= $last_request_row['action_on_date'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Admin:</td>
+                        <td>
+                            <?= $last_request_row['username'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Verdict:</td>
+                        <td>
+                            <?= $last_request_row['verdict'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Verdict Date:</td>
+                        <td>
+                            <?= $last_request_row['verdict_date'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Reseller Name:</td>
+                        <td>
+                            <?= $last_request_row['full_name'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Product Name:</td>
+                        <td>
+                            <?= $last_request_row['product_title'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Product price:</td>
+                        <td>
+                            <?= $last_request_row['product_price'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Product Type:</td>
+                        <td>
+                            <?= $last_request_row['product_subscription_type'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Product Category:</td>
+                        <td>
+                            <?= $last_request_row['product_category'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Note:</td>
+                        <td>
+                            <?= $last_request_row['note'] ?>
+                        </td>
+                    </tr>
+                    <?PHP }
+                    else{
+                      ?>
+                      <tr>
+                          <td>There are no previous Requests</td>
+
+                      </tr>
+                      <?PHP
+                    }?>
+                  </table>
+                </div>
+            </div>
+            </p>
+        </div>
+    </div>
+
+
 
 <?php
 include_once "../footer.php";
