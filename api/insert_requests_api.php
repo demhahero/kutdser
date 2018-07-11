4c461fa50006aac65692b60e756e06476d86a9f3
demhahero <ahmedsmail_91@yahoo.com>
2018-07-11 00:32:12 -0400

    in make request, disable the current product either from orders or requests table. Whichever is the most recent

--- api/insert_requests_api.php
+++ api/insert_requests_api.php
@@ -62,26 +62,33 @@ if (isset($_GET["order_id"]) && !isset($_GET['do'])) {
 }
 else if (isset($_GET["order_id"]) && isset($_GET['do']) == "product_list") {
     include_once "dbconfig.php";
-    
+
     $dbTools->query("SET CHARACTER SET utf8");
+
+    $getOrder = $dbTools->query("SELECT `orders`.`product_title`, `request_id`, `requests`.`product_title` as request_product_title, 
+                                `orders`.`product_subscription_type`, `orders`.`product_category`
+                                from orders 
+                                inner join order_options on orders.order_id= order_options.order_id
+                                left join requests on orders.order_id= requests.order_id 
+                                where orders.order_id=" . $_GET["order_id"]. " order by `request_id` desc limit 0,1");
     
-    $getOrder = $dbTools->query("SELECT * from orders inner join order_options on orders.order_id= order_options.order_id where  orders.order_id=" . $_GET["order_id"]);
     $order_row = $dbTools->fetch_assoc($getOrder);
-    
-    $getProducts = $dbTools->query("SELECT * from `products` where `subscription_type`='".$order_row["product_subscription_type"]."' "
-            . "and `category`='".$order_row["product_category"]."' and `status`='active' "
-            . "and `title` != '".$order_row["product_title"]."'");
+
+    $current_product_title = ($order_row["request_product_title"] != "")? $order_row["product_title"] : $order_row["request_product_title"];
+
+    $getProducts = $dbTools->query("SELECT * from `products` where `subscription_type`='" . $order_row["product_subscription_type"] . "' "
+            . "and `category`='" . $order_row["product_category"] . "' and `status`='active' "
+            . "and `title` != '" . $current_product_title . "'");
     $products = array();
-    while($product_row = $dbTools->fetch_assoc($getProducts)){
+    while ($product_row = $dbTools->fetch_assoc($getProducts)) {
         $products[] = $product_row;
     }
 
     $json = json_encode($products);
-    
+
     echo "{\"products\" :", $json, ",\"error\" :\"null\"}";
     die();
-}
-else if (isset($_POST["order_id"])) {
+} else if (isset($_POST["order_id"])) {
 
     include_once "dbconfig.php";
 
@@ -105,12 +112,12 @@ else if (isset($_POST["order_id"])) {
         "verdict" => "",
         "verdict_date" => "",
         "action_on_date" => "",
-        "modem_mac_address"=>"",
-        "modem_id"=>"",
-        "city"=>"",
-        "address_line_1"=>"",
-        "address_line_2"=>"",
-        "postal_code"=>"",
+        "modem_mac_address" => "",
+        "modem_id" => "",
+        "city" => "",
+        "address_line_1" => "",
+        "address_line_2" => "",
+        "postal_code" => "",
         "note" => "",
             //"product_title"=>"",
             //"product_category"=>"",
@@ -132,12 +139,12 @@ else if (isset($_POST["order_id"])) {
         "product_title" => "",
         "product_category" => "",
         "product_subscription_type" => "",
-        "modem_mac_address"=>"",
-        "modem_id"=>"",
-        "city"=>"",
-        "address_line_1"=>"",
-        "address_line_2"=>"",
-        "postal_code"=>"",
+        "modem_mac_address" => "",
+        "modem_id" => "",
+        "city" => "",
+        "address_line_1" => "",
+        "address_line_2" => "",
+        "postal_code" => "",
     );
 
     if (!isset($_POST["product_id"])) {
@@ -148,28 +155,12 @@ else if (isset($_POST["order_id"])) {
     foreach ($PostFields as $key => $value) {
         if (isset($_POST[$key])) {
             $InsertFieldValues[$key] = $_POST[$key];
-        } else if ($key === "verdict"
-                || $key === "verdict_date"
-                || $key === "action_value"
-                || $key === "admin_id"
-                || $key === "note"
-                || $key === "product_title"
-                || $key === "product_category"
-                || $key === "product_price"
-                || $key === "product_subscription_type"
-                || $key === "creation_date"
-                || $key === "modem_id"
-                || $key === "address_line_2"
-                || $key === "modem_mac_address") {
+        } else if ($key === "verdict" || $key === "verdict_date" || $key === "action_value" || $key === "admin_id" || $key === "note" || $key === "product_title" || $key === "product_category" || $key === "product_price" || $key === "product_subscription_type" || $key === "creation_date" || $key === "modem_id" || $key === "address_line_2" || $key === "modem_mac_address") {
             $InsertFieldValues[$key] = "";
-        }else if (isset($_POST["action"]) && $_POST["action"] !== "moving"
-                && ($key === "address_line_1"
-                    || $key === "city"
-                    || $key === "postal_code")
-                  ){
-          $InsertFieldValues[$key] = "";
-        }
-         else {
+        } else if (isset($_POST["action"]) && $_POST["action"] !== "moving" && ($key === "address_line_1" || $key === "city" || $key === "postal_code")
+        ) {
+            $InsertFieldValues[$key] = "";
+        } else {
             echo "{\"inserted\" :false,\"error\" :\"error: not all values sent in POST\"}";
             exit();
         }
@@ -207,7 +198,7 @@ else if (isset($_POST["order_id"])) {
     $values = "";
     foreach ($InsertFieldValues as $column => $value) {
         $columns .= "`" . $column . "`,";
-        if($value == "NULL")
+        if ($value == "NULL")
             $values .= "NULL,";
         else
             $values .= "N'" . $value . "',";
