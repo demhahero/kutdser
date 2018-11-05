0448e356b8367438956006a2dafa1a435d346eec
demhahero <ahmedsmail_91@yahoo.com>
2018-11-04 22:13:22 -0500

    fixed dbconfig request

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 //error_reporting(E_ALL);
@@ -14,76 +15,75 @@ $page = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
 
 if (strpos($_SERVER['REQUEST_URI'], 'mikrotik') !== false) {
 
-  include $_SERVER['DOCUMENT_ROOT']."/kutdser/api/tools/DBTools.php";
-  $dbTools = new DBTools($servername,$dbusername,$dbpassword,$dbname);
-  $site_url="http://localhost/kutdser/mikrotik";
-  $api_url = "http://localhost/kutdser/api/";
+    include $_SERVER['DOCUMENT_ROOT'] . "/kutdser/api/tools/DBTools.php";
+    $dbTools = new DBTools($servername, $dbusername, $dbpassword, $dbname);
+    $site_url = "http://localhost/kutdser/mikrotik";
+    $api_url = "http://localhost/kutdser/api/";
 
 
-  if ($page != "login.php") {
-      $session_id = stripslashes($_SESSION["session_id"]);
-      $admin_result = $dbTools->query("SELECT * FROM `admins` WHERE `session_id`='" . $session_id . "' AND `session_id`!=''");
-      if(!$admin_row = $admin_result->fetch_assoc()){
-          header('Location: '.$site_url.'/login.php');
-          die();
-      }
+    if ($page != "login.php") {
+        $session_id = stripslashes($_SESSION["session_id"]);
+        $admin_result = $dbTools->query("SELECT * FROM `admins` WHERE `session_id`='" . $session_id . "' AND `session_id`!=''");
+        if (!$admin_row = $admin_result->fetch_assoc()) {
+            header('Location: ' . $site_url . '/login.php');
+            die();
+        }
 
-      $username = $admin_row["username"];
-      $admin_id = $admin_row["admin_id"];
-  }
-}
-else if (strpos($_SERVER['REQUEST_URI'], 'ResellerPortal') !== false) {
-  include $_SERVER['DOCUMENT_ROOT']."/kutdser/api/tools/DBTools.php";
-  $dbTools = new DBTools($servername,$dbusername,$dbpassword,$dbname);
+        $username = $admin_row["username"];
+        $admin_id = $admin_row["admin_id"];
+    }
+} else if (strpos($_SERVER['REQUEST_URI'], 'ResellerPortal') !== false) {
+    include $_SERVER['DOCUMENT_ROOT'] . "/kutdser/api/tools/DBTools.php";
+    $dbTools = new DBTools($servername, $dbusername, $dbpassword, $dbname);
 
-  include $_SERVER['DOCUMENT_ROOT']."/kutdser/ResellerPortal/tools/DBTools.php";
-  $dbToolsReseller = new DBToolsReseller($servername,$dbusername,$dbpassword,$dbname);
-  $site_url="http://localhost/kutdser/ResellerPortal";
-  $api_url = "http://localhost/kutdser/api/";
-  if ($page != "login.php") {
+    include $_SERVER['DOCUMENT_ROOT'] . "/kutdser/ResellerPortal/tools/DBTools.php";
+    $dbToolsReseller = new DBToolsReseller($servername, $dbusername, $dbpassword, $dbname);
+    $site_url = "http://localhost/kutdser/ResellerPortal";
+    $api_url = "http://localhost/kutdser/api/";
+    if ($page != "login.php") {
 
-      $session_id =stripslashes($_SESSION["session_id"]);
+        $session_id = stripslashes($_SESSION["session_id"]);
 
-      if ($session_id == FALSE)
-          header('Location: '.$site_url.'/login.php');
+        if ($session_id == FALSE)
+            header('Location: ' . $site_url . '/login.php');
 
-      $query = $dbTools->query("SELECT * FROM `customers` WHERE `session_id`='" . $session_id . "'");
-      while ($row  = $query->fetch_assoc()) {
-          $username = $row["username"];
-          $reseller_id = $row["customer_id"];
-          if($row["is_new_system"] == "1")
-              $is_new_system = true;
-      }
-
-      if ($username == null) {
-          header('Location: '.$site_url.'/login.php');
-      }
-  }
-}
-else if (strpos($_SERVER['REQUEST_URI'], 'api') !== false) {
-  include $_SERVER['DOCUMENT_ROOT']."/kutdser/api/tools/DBTools.php";
-  $dbTools = new DBTools($servername,$dbusername,$dbpassword,$dbname);
-  $site_url="http://localhost/kutdser/mikrotik";
-  $api_url = "http://localhost/kutdser/api/";
-
-
-      $session_id = stripslashes($_SESSION["session_id"]);
-      $admin_result = $dbTools->query("SELECT * FROM `admins` WHERE `session_id`='" . $session_id . "' AND `session_id`!=''");
-      if(!$admin_row = $admin_result->fetch_assoc()){
         $query = $dbTools->query("SELECT * FROM `customers` WHERE `session_id`='" . $session_id . "'");
-        if(!$row  = $query->fetch_assoc()) {
-          echo "{\"error\":\"you do not have permission\"}";
+        while ($row = $query->fetch_assoc()) {
+            $username = $row["username"];
+            $reseller_id = $row["customer_id"];
+            if ($row["is_new_system"] == "1")
+                $is_new_system = true;
+        }
+
+        if ($username == null) {
+            header('Location: ' . $site_url . '/login.php');
+        }
+    }
+} else if (strpos($_SERVER['REQUEST_URI'], 'api') !== false) {
+
+    include $_SERVER['DOCUMENT_ROOT'] . "/kutdser/api/tools/DBTools.php";
+    $dbTools = new DBTools($servername, $dbusername, $dbpassword, $dbname);
+    $site_url = "http://localhost/kutdser/mikrotik";
+    $api_url = "http://localhost/kutdser/api/";
+
+
+    $session_id = stripslashes($_SESSION["session_id"]);
+    $admin_result = $dbTools->query("SELECT * FROM `admins` WHERE `session_id`='" . $session_id . "' AND `session_id`!=''");
+    
+    if (!$admin_row = $admin_result->fetch_assoc()) {
+        $query = $dbTools->query("SELECT * FROM `customers` WHERE `session_id`='" . $session_id . "'");
+        
+        if (!$row = $query->fetch_assoc()) {
+            echo "{\"error\":\"you do not have permission\"}";
             exit();
         }
         $username = $row["username"];
         $reseller_id = $row["customer_id"];
-        if($row["is_new_system"] == "1")
+        if ($row["is_new_system"] == "1")
             $is_new_system = true;
-            exit();
-      }
+    }
 
-      $username = $admin_row["username"];
-      $admin_id = $admin_row["admin_id"];
-
+    $username = $admin_row["username"];
+    $admin_id = $admin_row["admin_id"];
 }
 ?>
