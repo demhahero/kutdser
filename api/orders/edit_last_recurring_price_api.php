29fa685f93aebfdd2923bab54d224840d6b46ee5
demhahero <ahmedsmail_91@yahoo.com>
2019-06-02 18:22:37 -0400

    fix edit last recurring price, add prices and caution to avoid mistakes.

--- api/orders/edit_last_recurring_price_api.php
+++ api/orders/edit_last_recurring_price_api.php
@@ -2,11 +2,14 @@
 
 if (isset($_GET["order_id"]) /* && isset($_GET["action_on_date"]) */) {
     include_once "../dbconfig.php";
-
-    $order_query = "SELECT `orders`.*,`order_options`.*,`customers`.`full_name`,`orders`.`order_id` as `this_order_id` FROM `orders`
+    
+    $order_query = "SELECT `orders`.*,`order_options`.*,`customers`.`full_name`,`orders`.`order_id` as `this_order_id`,
+        `invoices`.`product_price` as `last_invoice_product_price` FROM `orders`
         INNER JOIN `order_options` on `order_options`.`order_id`=`orders`.`order_id`
         INNER JOIN `customers` on `customers`.`customer_id`=`orders`.`customer_id`
-        where `orders`.`order_id`=?";
+        INNER JOIN `invoices` on `invoices`.`order_id`=`orders`.`order_id` and `invoices`.`invoice_type_id` in (1,2,3)
+    
+        where `orders`.`order_id`=? order by `invoices`.valid_date_from desc";
 
     $stmt1 = $dbTools->getConnection()->prepare($order_query);
 
@@ -32,6 +35,14 @@ if (isset($_GET["order_id"]) /* && isset($_GET["action_on_date"]) */) {
             }
         }
         $result["start_active_date"] = $start_active_date;
+        
+        $converted = DateTime::createFromFormat("Y-m-d H:i:s", $result["start_active_date"]);
+        $converted1Year = $converted->add(new DateInterval("P1Y"));
+        
+        if($converted1Year->format("d") != "1")
+            $converted1Year->modify('first day of next month');
+        
+        $result["offer_end"] = $converted1Year->format("Y-m-d H:i:s");
         $json = json_encode($result);
         echo "{\"order\" :", $json, "}";
     }
@@ -62,10 +73,62 @@ if (isset($_GET["order_id"]) /* && isset($_GET["action_on_date"]) */) {
         $stmt1->execute();
         if($stmt1->errno != 0)
             $succeeded = FALSE;
+    
+        
+        $invoice_items_query = "SELECT * FROM `invoice_items` "
+            . "where `invoice_id`= ?  order by `invoice_item_id` asc";
+        $stmt1 = $dbTools->getConnection()->prepare($invoice_items_query);
+        $param_value = $invoice_row["invoice_id"];
+        $stmt1->bind_param('s', $param_value
+        ); // 's' specifies the variable type => 'string
+        $stmt1->execute();
+        $result1 = $stmt1->get_result();
+        
+        if($stmt1->errno != 0)
+                    $succeeded = FALSE;
+        
+        $total_price = 0;    
+
+        while($invoice_items_row = $dbTools->fetch_assoc($result1)){
+            if(substr($invoice_items_row["item_name"], 0, 7)=="product"){
+                $total_price+= $product_price;
+                $query = "UPDATE `invoice_items` SET `item_price`=?,"
+                . "`item_duration_price`=? WHERE `invoice_item_id`=?";
+                $stmt1 = $dbTools->getConnection()->prepare($query);
+                $stmt1->bind_param('sss', $product_price, $product_price, $invoice_items_row['invoice_item_id']);
+                $stmt1->execute();
+                
+                if($stmt1->errno != 0)
+                    $succeeded = FALSE;
+            } else if($invoice_items_row["item_name"] == "Router price"
+                    || $invoice_items_row["item_name"] == "Additional service price"
+                    || $invoice_items_row["item_name"] == "Static IP price"){
+                $total_price+= $invoice_items_row["item_price"];
+            } else if($invoice_items_row["item_name"] == "QST Tax"){
+                $qst = $total_price * 0.09975;
+                $query = "UPDATE `invoice_items` SET `item_price`=?,"
+                . "`item_duration_price`=? WHERE `invoice_item_id`=?";
+                $stmt1 = $dbTools->getConnection()->prepare($query);
+                $stmt1->bind_param('sss', $qst, $qst, $invoice_items_row['invoice_item_id']);
+                $stmt1->execute();
+                
+                if($stmt1->errno != 0)
+                    $succeeded = FALSE;
+            } else if($invoice_items_row["item_name"] == "GST Tax"){
+                $gst = $total_price * 0.05;
+                $query = "UPDATE `invoice_items` SET `item_price`=?,"
+                . "`item_duration_price`=? WHERE `invoice_item_id`=?";
+                $stmt1 = $dbTools->getConnection()->prepare($query);
+                $stmt1->bind_param('sss', $gst, $gst, $invoice_items_row['invoice_item_id']);
+                $stmt1->execute();
+                
+                if($stmt1->errno != 0)
+                    $succeeded = FALSE;
+            }
+        }
     }
 
-
-
+    
     if ($succeeded) {
         echo "{\"inserted\" :true,\"error\" :\"null\"}";
     } else {
--- mikrotik/orders/edit_last_recurring_price.php
+++ mikrotik/orders/edit_last_recurring_price.php
@@ -7,11 +7,19 @@ include_once "../header.php";
     $(document).ready(function () {
 
         $.getJSON("<?= $api_url ?>orders/edit_last_recurring_price_api.php?order_id=<?= $_GET["order_id"] ?>", function (result) {
-
+            $(".full-name").html(result["order"]["full_name"]);
             $(".product-name").html(result["order"]["product_title"]);
             $(".start-active-date").html(result["order"]["start_active_date"]);
+            $(".offer-end").html(result["order"]["offer_end"]);
+            
             $(".product-subscription-type").html(result["order"]["product_subscription_type"]);
-            $("input[name=\"product_price\"]").val(result["order"]["product_price"]);
+            $("input[name=\"product_price\"]").val(result["order"]["last_invoice_product_price"]);
+            
+            if(result["order"]["last_invoice_product_price"] != "39.9" 
+                    && result["order"]["last_invoice_product_price"] != "377.23"){
+                alert("CAREFULL, this order has been updated");
+                $(".caution").show();
+            }
         });
         
         $(".submit").click(function () {
@@ -61,19 +69,28 @@ include_once "../header.php";
     <a class="last" href="">Customer - Edit Recurring</a>
 </div>
 
+<p>Full Name: <b class="full-name"></b></p>
 <p>Product: <b class="product-name"></b></p>
-<p>Recurring Started on: <b class="start-active-date"></b></p>
+<p>First Payment: <b class="start-active-date"></b></p>
+<p>Offer ends on: <b class="offer-end"></b></p>
 <p>Subscription type: <b class="product-subscription-type"></b></p>
 
 <form class="register-form" method="post">
     <div class="form-group">
         <div class="form-group">
-            <label>Product price:</label>
+            <label>Product price: 
+                <br/><b style="color: gray;">Internet 30 Mbps MONTHLY 44.90$</b> 
+                <br/> <b style="color: gray;">Internet 30 Mbps YEARLY 538.90$</b> 
+                <br/> <b class="caution" style="display: none; color: red; font-size: 25px;">Caution! This price is not the original offer price!!!</b> 
+            </label>
+                
             <input type="text" name="product_price" class="form-control" />
         </div>
     </div>
     <input type="submit" class="btn btn-default submit"  value="Send">
 </form>
+
+
 
 <?php
 include_once "../footer.php";
